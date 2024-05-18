3894aebc83bd5d05290b8912e412243d01b8fbd8
R <jugle.orgx99@gmail.com>
2024-05-18 16:01:35 +0100

    10x larger

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 #[tokio::main]
 async fn main() {
-  const N: usize = 1000000;
+  const N: usize = 10000000;
   let mut ip: Vec<bool> = vec![true; N+1];
   let mut primes: Vec<usize> = vec![];
-  ip[1] = false;
   for i in 2..=N{
     if ip[i]{
       primes.push(i);
-      for j in (i*i..=N).step_by(i){
-        ip[j] = false;
+      let mut sq = i*i;
+      while sq <= N{
+        ip[sq] = false;
+        sq += i;
       }
     }
   }
-  print!("Number Of Primes: {}",primes.len())
+  print!("Number Of Primes: {}",primes.len());
 }
