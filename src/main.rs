2a7032527fa32ae24e465fdf0538c7c68ae5935c
R <jugle.orgx99@gmail.com>
2024-05-18 17:06:36 +0100

    take user input

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use std::{io::Write, io};
 #[tokio::main]
 async fn main() {
-  const N: usize = 10000000;
+  let N: usize = prompt("primes up to ");
   let mut ip: Vec<bool> = vec![true; N+1];
   let mut primes: Vec<usize> = vec![];
   for i in 2..=N{
@@ -14,4 +15,17 @@ async fn main() {
     }
   }
   print!("Number Of Primes: {}",primes.len());
+}
+fn prompt(text:&str) -> usize {
+    let mut input_line = String::new();
+    print!("{}", text);
+    std::io::stdout().flush().unwrap();
+    io::stdin() // the rough equivalent of `std::cin`
+        .read_line(&mut input_line) // actually read the line
+        .expect("Failed to read line"); // which can fail, however
+    let x: usize = input_line
+        .trim() // ignore whitespace around input
+        .parse() // convert to integers
+        .expect("Input not an integer"); // which, again, can fail
+    return x;
 }
